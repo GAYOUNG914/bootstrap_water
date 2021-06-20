0ab2f5dbe41ce3cf3f7e440de87bdb5e9129cc13
GAYOUNG914 <rkdud91455@gmail.com>
2021-06-21 01:23:18 +0900

    docs: add codes
    make a tag unactive & remove some codes for smoother interation

--- js/common.js
+++ js/common.js
@@ -10,14 +10,18 @@ $(function () {
       let thisOffset = $(this).offset();
       if (thisOffset.top <= thisScrollTop + addTop && thisScrollTop < thisOffset.top + wHeight) {
         $(this).addClass("on");
-      } else {
-        $(this).removeClass("on");
       }
     });
   });
 
+  //back to top btn
   $(".back_to_top a").click(function () {
     $("html,body").stop();
     $("html,body").animate({ scrollTop: 0 }, 300);
+  });
+
+  //a 태그 비활성화
+  $('a[href="#"]').click(function (e) {
+    e.preventDefault();
   });
 });
